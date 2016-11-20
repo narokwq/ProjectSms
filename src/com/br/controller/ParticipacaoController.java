0204f7d5de8794d7604096f61649224a563aca40
Alex <Alex@192.168.10.3>
2016-11-20 02:06:56 -0200

    bug long

--- src/com/br/controller/ParticipacaoController.java
+++ src/com/br/controller/ParticipacaoController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.br.model.Aluno;
 import com.br.model.Participacao;
 import com.br.services.AlunoService;
 import com.br.services.ParticipacaoService;
@@ -31,12 +30,12 @@ public class ParticipacaoController {
 		
 	}
 	@RequestMapping(method = RequestMethod.GET, value = "{keyhash}/{id}/pontuar")
-	public String addPontuacao(@PathVariable Long keyhash, @PathVariable Long id){
+	public String addPontuacao(@PathVariable String keyhash, @PathVariable Long id){
 		Participacao participacao = new Participacao();
 		participacao.setId(id);
 		particService.atulizarPontuacao(participacao);
 		
-		return "redirect:/sala"+keyhash+"/list";
+		return "redirect:/sala/"+keyhash+"/list";
 		
 	}
 }
